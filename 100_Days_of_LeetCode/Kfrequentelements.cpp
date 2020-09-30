be24a1df7b4f7b722172ff803018c9833e26999d
nidakhan786 <nidaslife23@gmail.com>
2020-09-30 23:42:07 +0530

    [Day-01]:K frequent elements

--- 100_Days_of_LeetCode/Kfrequentelements.cpp
+++ 100_Days_of_LeetCode/Kfrequentelements.cpp
@@ -1,36 +1,39 @@
 // Implementation of K frequent elements occurs in array in C++
 #include <iostream>
-#include<vector>
-#include<unordered_map>
-#include <utility> 
+#include <vector>
+#include <unordered_map>
+#include <utility>
 #include <bits/stdc++.h>
 using namespace std;
 
-bool compare(pair<int,int>p1, pair<int,int> p2){
-if(p1.second == p2.second)
-    return p1.first>p2.first;
+bool compare(pair<int, int> p1, pair<int, int> p2)
+{
+    if (p1.second == p2.second)
+        return p1.first > p2.first;
 
-
-return p1.second>p2.second;
+    return p1.second > p2.second;
 }
-void printFrequentElements(int arr[], int n, int k){
+void printFrequentElements(int arr[], int n, int k)
+{
 
     unordered_map<int, int> freq;
-    for(int i =0; i<n ;i++){
+    for (int i = 0; i < n; i++)
+    {
         freq[arr[i]]++;
     }
- vector<pair<int, int> > freqarr(freq.begin(), freq.end());
-  sort(freqarr.begin(), freqarr.end(), compare);
-cout<< k + "frequently occuring numbers are:";
-  for(int i=0; i<k; i++){
-     cout<<freqarr[i].first;
-  }
+    vector<pair<int, int>> freqarr(freq.begin(), freq.end());
+    sort(freqarr.begin(), freqarr.end(), compare);
+    cout << k + "frequently occuring numbers are:";
+    for (int i = 0; i < k; i++)
+    {
+        cout << freqarr[i].first;
+    }
 }
-int main() 
-{ 
-    int arr[] = { 1,2,4,5,5,5,5,5,3,3,2,2}; 
-    int n = sizeof(arr) / sizeof(arr[0]); 
-    int k = 2; 
-    printFrequentElements(arr, n, k); 
-    return 0; 
-} 
+int main()
+{
+    int arr[] = {1, 2, 4, 5, 5, 5, 5, 5, 3, 3, 2, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int k = 2;
+    printFrequentElements(arr, n, k);
+    return 0;
+}
